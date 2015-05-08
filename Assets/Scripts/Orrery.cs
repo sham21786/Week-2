5c9ae12b24064e6d3ced9ad4c766ce1cbafb5ad3
Shazmil Nassar <sham21786@yahoo.com>
2015-05-08 13:56:40 +0100

    Week 2

---
+++ Assets/Scripts/Orrery.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orrery : MonoBehaviour {
+
+    public Transform sun;
+    public Transform earth;
+    public Transform moon;
+
+	void Update () {
+        moon.transform.RotateAround(earth.transform.position, Vector3.up, Time.deltaTime * 365);
+        earth.transform.RotateAround(sun.transform.position, Vector3.up, Time.deltaTime * 24);
+	}
+}
